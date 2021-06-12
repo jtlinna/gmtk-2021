f526ea9fec2c8b29a1f734a285fed544e10e2dbc
jtlinna <jukka.linna@gmail.com>
2021-06-12 18:12:55 +0300

    Added simple pause menu

---
+++ Assets/Scripts/PauseMenuHandler.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public sealed class PauseMenuHandler : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _pauseMenuScreen = null;
+
+    private bool _isPaused;
+
+    private void Start()
+    {
+        InputManager.ControlScheme.Yeet.PauseGame.performed += OnPauseGame;
+        _isPaused = false;
+        if(_pauseMenuScreen != null)
+        {
+            _pauseMenuScreen.SetActive(false);
+        }
+    }
+
+    public void SetPaused(bool isPaused)
+    {
+        _isPaused = isPaused;
+        GameManager.Instance.SetPaused(isPaused);
+        if(_pauseMenuScreen == null)
+        {
+            return;
+        }
+
+        _pauseMenuScreen.SetActive(_isPaused);
+    }
+
+    private void OnDestroy()
+    {
+        InputManager.ControlScheme.Yeet.PauseGame.performed -= OnPauseGame;
+    }
+
+    private void OnPauseGame(InputAction.CallbackContext obj) => SetPaused(!_isPaused);
+}
