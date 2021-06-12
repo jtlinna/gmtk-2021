0b61fa0030096e4a516aba3907ca7eefb98a849f
jtlinna <jukka.linna@gmail.com>
2021-06-12 11:58:26 +0300

    Added simple character effect handler

---
+++ Assets/Scripts/CharacterEffectHandler.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public sealed class CharacterEffectHandler : MonoBehaviour
+{
+    [Serializable]
+    private class EffectTuple
+    {
+        [SerializeField]
+        private PowerUpIdentifier _id;
+        public PowerUpIdentifier Id => _id;
+
+        [SerializeField]
+        private GameObject _effectObject;
+        public GameObject EffectObject => _effectObject;
+    }
+
+    [SerializeField]
+    private List<EffectTuple> _effects = new List<EffectTuple>();
+
+    private void Start()
+    {
+        GameManager.Instance.PowerUpRegistered += OnPowerUpRegistered;
+        GameManager.Instance.PowerUpUnregistered += OnPowerUpUnregistered;
+
+        _effects.ForEach(DisableEffect);
+    }
+
+    private void OnDestroy()
+    {
+        if(GameManager.Instance == null)
+        {
+            return;
+        }
+
+        GameManager.Instance.PowerUpRegistered -= OnPowerUpRegistered;
+        GameManager.Instance.PowerUpUnregistered -= OnPowerUpUnregistered;
+    }
+
+    private void EnableEffect(EffectTuple effect)
+    {
+        if(effect?.EffectObject != null)
+        {
+            effect.EffectObject.SetActive(true);
+        }
+    }
+
+    private void DisableEffect(EffectTuple effect)
+    {
+        if(effect?.EffectObject != null)
+        {
+            effect.EffectObject.SetActive(false);
+        }
+    }
+
+    private void OnPowerUpRegistered(BasePowerUp powerUp) => EnableEffect(_effects.FirstOrDefault(e => e.Id == powerUp.Id));
+    private void OnPowerUpUnregistered(BasePowerUp powerUp) => DisableEffect(_effects.FirstOrDefault(e => e.Id == powerUp.Id));
+}
