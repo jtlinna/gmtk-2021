d546e7a817353ef519a2e4fa6a736f364255b03e
jtlinna <jukka.linna@gmail.com>
2021-06-12 10:30:06 +0300

    Fix power ups to register when first char enters & unregister when last char exits

--- Assets/Scripts/BasePowerUp.cs
+++ Assets/Scripts/BasePowerUp.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class BasePowerUp : MonoBehaviour
 {
@@ -6,9 +7,29 @@ public class BasePowerUp : MonoBehaviour
     private PowerUpIdentifier _id;
     public PowerUpIdentifier Id => _id;
 
+    private HashSet<Character> _affectedCharacters = new HashSet<Character>();
+
+    private void Awake()
+    {
+        _affectedCharacters = new HashSet<Character>();
+    }
+
+    private void OnDestroy()
+    {
+        _affectedCharacters.Clear();
+    }
+
     protected virtual void OnTriggerEnter(Collider collider)
     {
-        if(collider.GetComponentInParent<Character>() != null)
+        Character character = collider.GetComponentInParent<Character>();
+        if(collider == null)
+        {
+            return;
+        }
+
+        int prevCount = _affectedCharacters.Count;
+        _affectedCharacters.Add(character);
+        if(prevCount == 0)
         {
             GameManager.Instance.RegisterPowerUp(this);
         }
@@ -16,7 +37,14 @@ public class BasePowerUp : MonoBehaviour
 
     protected virtual void OnTriggerExit(Collider collider)
     {
-        if(collider.GetComponentInParent<Character>() != null)
+        Character character = collider.GetComponentInParent<Character>();
+        if(collider == null)
+        {
+            return;
+        }
+
+        _affectedCharacters.Remove(character);
+        if(_affectedCharacters.Count == 0)
         {
             GameManager.Instance.UnregisterPowerUp(this);
         }
