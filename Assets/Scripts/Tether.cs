cde0fb6879771090f2f9ee791d75fb0d7e19d880
jtlinna <jukka.linna@gmail.com>
2021-06-13 15:29:09 +0300

    Added multi-character support to tethers

--- Assets/Scripts/Tether.cs
+++ Assets/Scripts/Tether.cs
@@ -1,135 +1,145 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
+using System.Linq;
 using UnityEngine;
-using Random = UnityEngine.Random;
 
 public class Tether : MonoBehaviour
 {
+    [Serializable]
+    private class TetherHandler
+    {
+
+        [SerializeField]
+        private LineRenderer _lineRenderer;
+
+        [SerializeField]
+        private Transform _targetPointTransform;
+
+        public bool IsConnected { get; set; }
+        public bool IsTethered { get; set; }
+        public float MiddleYPosition { get; set; }
+        public float LerpCounter { get; set; } = 0f;
+        public Character ConnectedCharacter { get; set; }
+
+        private readonly List<Vector3> _pointList = new List<Vector3>();
+
+        private Vector3 _middlePoint = new Vector3();
+        private Vector3 _currentVelocity;
+        private float _distanceThreshold = 0.1f;
+
+        public void HandleFixedUpdate(AnimationCurve curve, Transform originPointTransform, float vertexCount, float maxSpeed, float maxDistance, float smoothTime)
+        {
+            if(IsConnected)
+            {
+                MiddleYPosition =
+                    curve.Evaluate(Vector3.Distance(_targetPointTransform.position, originPointTransform.position) /
+                                   maxDistance);
+                if(IsTethered)
+                {
+                    _targetPointTransform.position = ConnectedCharacter.GetTetherTransform.position;
+                }
+                else
+                {
+                    _targetPointTransform.position = Vector3.Lerp(_targetPointTransform.position, ConnectedCharacter.GetTetherTransform.position, LerpCounter += Time.fixedDeltaTime / 5);
+                    if(Vector3.Distance(_targetPointTransform.position, ConnectedCharacter.GetTetherTransform.position) <= _distanceThreshold)
+                    {
+                        IsTethered = true;
+                    }
+                }
+            }
+            else if(!IsTethered)
+            {
+                _targetPointTransform.position = Vector3.Lerp(_targetPointTransform.position, originPointTransform.position, LerpCounter += Time.fixedDeltaTime / 5);
+                if(Vector3.Distance(_targetPointTransform.position, originPointTransform.position) <= _distanceThreshold)
+                {
+                    IsTethered = true;
+                }
+            }
+
+            _pointList.Clear();
+
+            _middlePoint = Vector3.SmoothDamp(_middlePoint,
+                new Vector3((originPointTransform.transform.position.x + _targetPointTransform.transform.position.x) / 2,
+                    MiddleYPosition,
+                    (originPointTransform.transform.position.z + _targetPointTransform.transform.position.z) / 2),
+                ref _currentVelocity, smoothTime, maxSpeed, Time.fixedDeltaTime);
+
+            for(float ratio = 0; ratio <= 1; ratio += 1 / vertexCount)
+            {
+                Vector3 tangent1 = Vector3.Lerp(originPointTransform.position, _middlePoint, ratio);
+                Vector3 tangent2 = Vector3.Lerp(_middlePoint, _targetPointTransform.position, ratio);
+                Vector3 curveValue = Vector3.Lerp(tangent1, tangent2, ratio);
+
+                _pointList.Add(curveValue);
+            }
+
+
+            _lineRenderer.positionCount = _pointList.Count;
+
+
+            _lineRenderer.SetPositions(_pointList.ToArray());
+        }
+    }
+
+
     [SerializeField]
     private Transform _originPointTransform;
-
-    private Vector3 _middlePoint = new Vector3();
-
-    [SerializeField]
-    private Transform _targetPointTransform;
-    
-    private Character _connectedCharacter;
-    
-    [SerializeField]
-    private LineRenderer _linerenderer;
-
     [SerializeField]
     private float _vertexCount = 12;
-
-    private float _middleYposition = 2f;
-
     [SerializeField]
     private float _smoothTime = 2;
-
     [SerializeField]
     private float _maxSpeed = 2;
-
     [SerializeField]
     private AnimationCurve curve = null;
-
-    private Transform _originalPoint3;
-    private List<Transform> _point3Overrides = new List<Transform>();
-
     [SerializeField]
     private float MaxDistance;
+    [SerializeField]
+    private List<TetherHandler> _tetherHandlers = new List<TetherHandler>();
 
-    private bool _isConnected;
-    private bool _isTethered;
-    private float _lerpCounter = 0f;
-    private float _distanceThreshold = 0.1f;
+    private readonly Dictionary<Character, TetherHandler> _tetherHandlerLookup = new Dictionary<Character, TetherHandler>(); 
 
 
     public void OnCharacterEnter(Character character)
     {
-        if (_point3Overrides.Contains(character.transform))
+        if(_tetherHandlerLookup.ContainsKey(character))
         {
             return;
         }
-        _point3Overrides.Add(character.transform);
-        _isConnected = true;
-        _connectedCharacter = character;
-        _lerpCounter = 0;
-        _isTethered = false;
+
+        TetherHandler handler = _tetherHandlers.FirstOrDefault(h => !h.IsConnected);
+        if(handler == null)
+        {
+            return;
+        }
+
+        handler.IsConnected = true;
+        handler.ConnectedCharacter = character;
+        handler.LerpCounter = 0;
+        handler.IsTethered = false;
+        _tetherHandlerLookup[character] = handler;
     }
 
     public void OnCharacterExit(Character character)
     {
-        if (!_point3Overrides.Contains(character.transform))
+        if(!_tetherHandlerLookup.TryGetValue(character, out TetherHandler handler))
         {
             return;
         }
-        
-        _connectedCharacter = null;
-        _middleYposition = 1f;
-        _point3Overrides.Remove(character.transform);
-        _isConnected = false;
-        _lerpCounter = 0f;
-        _middleYposition = 1f;
-        _isTethered = false;
-    }
 
-    private Vector3 _currentVelocity;
-    private Vector3 _returningVelocity;
-    private List<Vector3> _pointList = new List<Vector3>();
+        handler.ConnectedCharacter = null;
+        handler.MiddleYPosition = 1f;
+        handler.IsConnected = false;
+        handler.IsTethered = false;
+        handler.LerpCounter = 0f;
+        _tetherHandlerLookup.Remove(character);
+    }
 
     void FixedUpdate()
     {
-       
-        if (_isConnected)
+        foreach(TetherHandler handler in _tetherHandlers)
         {
-            _middleYposition =
-                curve.Evaluate(Vector3.Distance(_targetPointTransform.position, _originPointTransform.position) /
-                               MaxDistance);
-            if (_isTethered)
-            {
-                _targetPointTransform.position = _connectedCharacter.GetTetherTransform.position;
-            }
-            else
-            {
-                _targetPointTransform.position = Vector3.Lerp(_targetPointTransform.position, _connectedCharacter.GetTetherTransform.position, _lerpCounter += Time.fixedDeltaTime / 5);
-                if (Vector3.Distance(_targetPointTransform.position, _connectedCharacter.GetTetherTransform.position) <= _distanceThreshold)
-                {
-                    _isTethered = true;
-                }
-            }
+            handler.HandleFixedUpdate(curve, _originPointTransform, _vertexCount, _maxSpeed, MaxDistance, _smoothTime);
         }
-        else if(!_isTethered)
-        {
-            _targetPointTransform.position = Vector3.Lerp(_targetPointTransform.position, _originPointTransform.position, _lerpCounter += Time.fixedDeltaTime / 5);
-            if (Vector3.Distance(_targetPointTransform.position, _originPointTransform.position) <= _distanceThreshold)
-            {
-                _isTethered = true;
-            }
-        }
-
-        _pointList = new List<Vector3>();
-
-        _middlePoint = Vector3.SmoothDamp(_middlePoint,
-            new Vector3((_originPointTransform.transform.position.x + _targetPointTransform.transform.position.x) / 2,
-                _middleYposition,
-                (_originPointTransform.transform.position.z + _targetPointTransform.transform.position.z) / 2),
-            ref _currentVelocity, _smoothTime, _maxSpeed, Time.fixedDeltaTime);
-
-        for (float ratio = 0; ratio <= 1; ratio += 1 / _vertexCount)
-        {
-            Vector3 tangent1 = Vector3.Lerp(_originPointTransform.position, _middlePoint, ratio);
-            Vector3 tangent2 = Vector3.Lerp(_middlePoint, _targetPointTransform.position, ratio);
-            Vector3 curve = Vector3.Lerp(tangent1, tangent2, ratio);
-
-            _pointList.Add(curve);
-        }
-
-
-        _linerenderer.positionCount = _pointList.Count;
-
-
-        _linerenderer.SetPositions(_pointList.ToArray());
     }
 }
